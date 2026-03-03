7734c2d91a830b7b6cae4b7079f53343af891bfb
Bolice1 <komezusengebolice@gmail.com>
2026-03-03 13:53:03 +0200

    correction of the nineth question

--- integerArray.c
+++ integerArray.c
@@ -1,18 +1,27 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
+int main() {
+
     int array[5] = {1,2,3,4,5};
 
-    int *prtelement = array;
+    int *ptr = array;
 
-    int n = sizeOf(array)/sizeof(array[0]) -1;
+    int n = sizeof(array) / sizeof(array[0]);
 
     int temp;
 
-    for(int i = 0;i<=n;i++){
-      
+    for(int i = 0; i < n / 2; i++) {
+
+        temp = *(ptr + i);
+        *(ptr + i) = *(ptr + n - 1 - i);
+        *(ptr + n - 1 - i) = temp;
+    }
+
+    // Print reversed array
+    for(int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
     }
 
     return 0;
-}
+} 
