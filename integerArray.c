3eb056f5e10f982dc271cfd26f8b1b89b9fb2159
Bolice1 <komezusengebolice@gmail.com>
2026-03-03 13:47:38 +0200

    correction of the tenth question

---
+++ integerArray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    int array[5] = {1,2,3,4,5};
+
+    int *prtelement = array;
+
+    int n = sizeOf(array)/sizeof(array[0]) -1;
+
+    int temp;
+
+    for(int i = 0;i<=n;i++){
+      
+    }
+
+    return 0;
+}
