e7d98c6c20ec7b33207a6ec66a08271dd874a1f3
Bolice1 <komezusengebolice@gmail.com>
2026-03-15 17:35:21 +0200

    swagger comments configurations

--- backend development/App.js
+++ backend development/App.js
@@ -2,34 +2,60 @@ import express from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
-
 // let us setup the express app 
 
 const app = express();
 
+// set up of the swagger options 
+
+const swaggerOptions = {
+    definition: {
+        info: {
+            title: "Library APIs",
+            version: "1.0.0"
+        }
+
+    },
+    apis: ['App.js'],
+}
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions)
+console.log(swaggerDocs)
+
+// swagger comments 
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: get all the books 
+ *     responses:
+ *        200: 
+ *          description: success
+ * 
+ */
 // let us try to handle simple crud requests 
 
-app.get("/books",(req,res)=>{
-res.status(200).send({msg:"Welcome to the books store"})
+app.get("/books", (req, res) => {
+    res.status(200).send({ msg: "Welcome to the books store" })
 })
 // getting books according to their id 
 
-app.get("/books",(req,res)=>{
-const bookID = req.params
+app.get("/books", (req, res) => {
+    const bookID = req.params
 
-// check if it exists from the db 
+    // check if it exists from the db 
 
-const exists = 
-res.status(200).send({msg:"Welcome to the books store"})
+    const exists =
+        res.status(200).send({ msg: "Welcome to the books store" })
 })
 
 // let us set up the port on which our program will run on 
 
 const PORT = process.env.PORT || 9783;
 
-app.listen(PORT,()=>{
-    (error)=>{
-        if (error){
+app.listen(PORT, () => {
+    (error) => {
+        if (error) {
             console.log(`${error.error}`)
         }
     }
