2d705e2f4ccd057d7b7cdb18a90e7871f219002c
Bolice1 <komezusengebolice@gmail.com>
2026-03-15 17:08:14 +0200

    simple changes to the files in the repo

--- backend development/App.js
+++ backend development/App.js
@@ -2,6 +2,19 @@ import express from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
+// let us setup the express app 
+
+const app = express();
+
 // let us set up the port on which our program will run on 
 
 const PORT = process.env.PORT || 9783;
+
+app.listen(PORT,()=>{
+    (error)=>{
+        if (error){
+            console.log(`${error.error}`)
+        }
+    }
+    console.log(`Our program is runnig on the port: ${PORT}`)
+})
