7731d325636519ff337c862e1963eeb9d2b587b3
Bolice1 <komezusengebolice@gmail.com>
2026-03-15 17:20:50 +0200

    simple changes

--- backend development/App.js
+++ backend development/App.js
@@ -2,9 +2,26 @@ import express from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
+
 // let us setup the express app 
 
 const app = express();
+
+// let us try to handle simple crud requests 
+
+app.get("/books",(req,res)=>{
+res.status(200).send({msg:"Welcome to the books store"})
+})
+// getting books according to their id 
+
+app.get("/books",(req,res)=>{
+const bookID = req.params
+
+// check if it exists from the db 
+
+const exists = 
+res.status(200).send({msg:"Welcome to the books store"})
+})
 
 // let us set up the port on which our program will run on 
 
