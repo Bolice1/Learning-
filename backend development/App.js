7fb9957f668b58ce32de4cea2aed07dacbab11d3
Bolice1 <komezusengebolice@gmail.com>
2026-03-15 17:01:28 +0200

    configuration of the package.json file

---
+++ backend development/App.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+// let us set up the port on which our program will run on 
+
+const PORT = process.env.PORT || 9783;
